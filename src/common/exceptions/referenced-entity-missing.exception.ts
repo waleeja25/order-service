d0e158cb4d494b80c89d165eb2227a27d3a5f98f
Waleeja Ali Rana <waleeja.ali25@gmail.com>
2026-08-15 22:00:58 +0500

    refactor: for shared package

--- src/common/exceptions/referenced-entity-missing.exception.ts
+++ src/common/exceptions/referenced-entity-missing.exception.ts
@@ -1,10 +1,12 @@
 import { DomainException } from './domain.exception';
+import { status as GrpcStatus } from '@grpc/grpc-js';
 
 export class ReferencedEntityMissingException extends DomainException {
   constructor(entityName: string, id: number) {
     super(
       'REFERENCED_ENTITY_MISSING',
       `${entityName} with id ${id} does not exist`,
+      GrpcStatus.INVALID_ARGUMENT,
     );
   }
 }
--- src/common/filters/domain-exception.filter.ts
+++ src/common/filters/domain-exception.filter.ts
@@ -1,34 +1,22 @@
 import { Catch, RpcExceptionFilter } from '@nestjs/common';
-import { status as GrpcStatus } from '@grpc/grpc-js';
 import { Observable, throwError } from 'rxjs';
 
 import { DomainException } from '../exceptions';
 import { grpcError } from './grpc-error';
 
 @Catch(DomainException)
-export class DomainExceptionFilter implements RpcExceptionFilter<DomainException> {
+export class DomainExceptionFilter
+  implements RpcExceptionFilter<DomainException>
+{
   catch(exception: DomainException): Observable<never> {
     return throwError(() =>
       grpcError(
-        this.getGrpcStatus(exception.code),
+        exception.grpcStatus,
         JSON.stringify({
           code: exception.code,
           message: exception.message,
         }),
       ),
     );
-  }
-
-  private getGrpcStatus(code: string): number {
-    switch (code) {
-      case 'ENTITY_NOT_FOUND':
-        return GrpcStatus.NOT_FOUND;
-
-      case 'REFERENCED_ENTITY_MISSING':
-        return GrpcStatus.INVALID_ARGUMENT;
-
-      default:
-        return GrpcStatus.INTERNAL;
-    }
   }
 }
